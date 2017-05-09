22c8bba1a3ec48225a8eab89d9887ff8205f5297
Dragon Master <ngrato@gmail.com>
2017-05-09 14:07:56 -0700

    updating with reactive form and comments

--- app/customers/customer.component.ts
+++ app/customers/customer.component.ts
@@ -1,17 +1,54 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl,  Validators } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
-    customer: Customer= new Customer();
+export class CustomerComponent implements OnInit {
+    customerForm: FormGroup;
+    customer: Customer = new Customer();
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    constructor(private fb: FormBuilder){}
+
+    ngOnInit(){
+        this.customerForm = this.fb.group({
+            firstName: ['Brain',[Validators.required, Validators.minLength(2)]],
+            // Each value property can take an array, the array takes two objects (1.)Is an object that has initial value and is if it disabled or not
+            // (2.) This is an onject of custome or build in validation guards.
+            // lastName: [{value:'n/a', disabled: true }],
+            lastName: ['Holiday',[Validators.required, Validators.maxLength(50)]],
+            email: ['',[Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+            sendCatalog:[{value:true}]
+        });
+
+        
+    }
+    
+    //this is a test button to show how you can populate the whole object model.
+    populateTestData(): void{
+        this.customerForm.setValue({
+            firstName: 'Brain',
+            lastName: 'Holiday',
+            email: 'bhiliday@gmail.com',
+            sendCatalog:false
+        })
+    }
+
+    // this is an example to populate SOME of the data. If model object is complete use function above called "setValue()"
+    populateSomeTestData(): void{
+        this.customerForm.patchValue({
+            firstName: 'Mary',
+            lastName: 'Telsa',
+            sendCatalog:true
+        })
+    }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
     }
  }
+
+
